ded564773467fcc518470cf2bc00483db5211e6c
YangTuanAnh <yangtuananh2003@gmail.com>
2023-03-08 11:14:54 +0700

    add: About content

--- pages/About.py
+++ pages/About.py
@@ -3,7 +3,19 @@ import numpy as np
 import pandas as pd
 
 '# About'
-'## Why'
-'## How'
-'## What'
+
+'## Why are we doing this?'
+'* Language or jargon is hard to understand '
+'* Lack of background knowledge or context '
+'* Lengthy or complex documents'
+'* Outdated knowledge that needs to be updated'
+
+'## How are we doing this?'
+'* Adjusting the difficulties of a document'
+'* Generating quizzes for testing based on the document'
+
+'## What are we benefitting from this?'
+'Improved learning experience'
+'Increased knowledge retention'
+'Time-saving'
 
